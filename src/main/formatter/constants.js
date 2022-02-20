1b944c9c627eb6a206d3d1544d3e3d7d20a7f9cb
seth <sethcaparelli@gmail.com>
2022-02-20 00:51:11 -0700

    Revert "Initial commit"
    This reverts commit 6c1e4d15523f7b41b2fb066b15a9874593d494d4.

--- src/main/formatter/constants.js
+++
@@ -1,8 +0,0 @@
-const REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
-
-exports.REGEX = REGEX
-exports.MUSIC_DIRECTORY = "/Users/seth/workspace/my-apps/music-formatter/src/test/resources/test-music-directory";
-exports.WEB_URL_REGEX = new RegExp(REGEX);
-exports.UNKNOWN_ARTIST = "Unknown Artist";
-exports.UNKNOWN_ALBUM = "Unknown Album";
-exports.VARIOUS_ARTISTS = "Various Artists";
